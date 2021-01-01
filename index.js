b30181fb6e292d7dcac93c9def3607c2b5f1ec36
Ibrahim Nour <ibrahamnour22@gmail.com>
2021-01-01 23:27:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Code your solutions in this file
+function writeCards(a){
+  let thanksArray[];
+  for (let i in a){
+    console.log(`Thank you, ${a[i]}, for the wonderful surprise gift!`)
+  }
+}
+writeCards(["Lisa", "Kaitlin", "Jan"]);
